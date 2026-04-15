f562c2478fb9dd8cf313bb7e3527ef0c5c204b3b
wwwzain1768-cmyk <www.zain1768@gmail.com>
2026-04-15 23:31:35 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+// sw.js
+const CACHE_NAME = 'v1-tower-cache';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icon-192x192.png',
+    './icon-512x512.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/localforage/1.10.0/localforage.min.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).catch(() => {
+                    // في حالة فشل الجلب (أوفلاين) وكان الطلب لصفحة HTML
+                    if (event.request.headers.get('accept').includes('text/html')) {
+                        return caches.match('./index.html');
+                    }
+                });
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
